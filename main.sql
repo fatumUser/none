43ef0eeffd2ead4c548d61e3ec0336116187d4eb
FantomIT <129615173+fatumUser@users.noreply.github.com>
2023-09-05 18:41:52 +0300

    Update main.sql

--- main.sql
+++ main.sql
@@ -2,9 +2,9 @@ CREATE DATABASE doc;
 use doc;
 
 CREATE TABLE docs(
-Id int not null AUTO_INCREMENT
-Name varchar(100) NULL
-Age varchar(100) NULL
+Id int not null AUTO_INCREMENT,
+Name varchar(100) NULL,
+Age varchar(100) NULL,
 PRIMARY KEY (Id)
 );
 
